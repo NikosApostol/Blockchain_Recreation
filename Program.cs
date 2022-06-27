f047fa0d7ebb7c23579b1529ab761319b47aaea0
NikosApostol <nikosapostol1997@gmail.com>
2022-06-27 17:34:34 +0300

    4th push

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace MyConsoleApp // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine("1. Create a new BlockChain");
                 Console.WriteLine("2. Add to an existing BlockChain");
+                Console.WriteLine("3. Delete a certain block from an existing BlockChain");
                 int.TryParse(Console.ReadLine(), out int choise);
 
                 switch (choise)
@@ -43,8 +44,18 @@ namespace MyConsoleApp // Note: actual namespace depends on the project name.
                                 Console.WriteLine("A new block was added successfully.");
                             break;
                         }
-                    default:
-                        break;
+                    case 3:
+                        {
+                            string folderName;
+                            do
+                            {
+                                Console.WriteLine("Please specify the name of the folder from where you want a certain block to be deleted.");
+                                folderName = Console.ReadLine().Trim();
+                            } while (folderName == "");
+                            if (CreateBlockchain(folderName, choise, dataTransaction))
+                                Console.WriteLine("Block deleted successfully!");
+                            break;
+                        }
                 }
 
                 do
@@ -77,19 +88,15 @@ namespace MyConsoleApp // Note: actual namespace depends on the project name.
                     operationCompleted = false;
                 }
             }
-            else
+            else if (choise == 2)
             {
                 if (Directory.Exists(direcrory))
                 {
                     filePaths = Directory.GetFiles(direcrory, "*.txt").ToList();
+                    if (!CheckBlockchainValidity(direcrory, filePaths))
+                        return false;
                     string lastFilePath = filePaths.Last();
-                    string pattern = @"\\";
-                    string[] elements = System.Text.RegularExpressions.Regex.Split(lastFilePath, pattern);
-                    blockName = elements.Last();
-                    string pattern2 = "_";
-                    string[] elements2 = System.Text.RegularExpressions.Regex.Split(blockName, pattern2);
-                    blockNumber = elements2.First();
-                    int.TryParse(blockNumber, out int number);
+                    var number = FindBlockNumber(direcrory, lastFilePath);
                     number++;
                     fullFilePath = direcrory + @"\" + number + "_Block.txt";
                     CreateBlockFile(fullFilePath, lastFilePath, false, transactionData);
@@ -100,7 +107,26 @@ namespace MyConsoleApp // Note: actual namespace depends on the project name.
                     operationCompleted = false;
                 }
             }
-            ProofOfWork();
+            else
+            {
+                if (Directory.Exists(direcrory))
+                {
+                    filePaths = Directory.GetFiles(direcrory, "*.txt").ToList();
+                    if (!CheckBlockchainValidity(direcrory, filePaths))
+                        return false;
+                    Console.WriteLine("This blockchain contains: " + filePaths.Count + " blocks. Which one do you want to delete?");
+                    int.TryParse(Console.ReadLine(), out int blockOfChoise);
+                    var kot = filePaths.Where(x => x.Contains(blockOfChoise+"_Block")).First();
+                    File.Delete(kot);
+                }
+                else
+                {
+                    Console.WriteLine("This folder does not exist");
+                    operationCompleted = false;
+                }
+            }
+            if(operationCompleted)
+                ProofOfWork();
             return operationCompleted;
         }
 
@@ -122,6 +148,34 @@ namespace MyConsoleApp // Note: actual namespace depends on the project name.
                 data = new UTF8Encoding(true).GetBytes(GetTransactionData(id, transactionData));
                 fs.Write(data, 0, data.Length);
             }
+        }
+
+        public static bool CheckBlockchainValidity(string direcrory, List<string> filePaths)
+        {
+            var counterOfBlocks = 0;
+            foreach (var block in filePaths)
+            {
+                var number1 = FindBlockNumber(direcrory, block);
+                counterOfBlocks++;
+                if (number1 != counterOfBlocks)
+                {
+                    Console.WriteLine("This blockchain is corrupted.");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int FindBlockNumber(string direcrory, string filePath)
+        {
+            string pattern = @"\\";
+            string[] elements = System.Text.RegularExpressions.Regex.Split(filePath, pattern);
+            var blockName = elements.Last();
+            string pattern2 = "_";
+            string[] elements2 = System.Text.RegularExpressions.Regex.Split(blockName, pattern2);
+            var blockNumber = elements2.First();
+            int.TryParse(blockNumber, out int number);
+            return number;
         }
 
         public static string GetTransactionData(int id, Dictionary<int, string> transactionData)
